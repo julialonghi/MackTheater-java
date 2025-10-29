90521aeb728ee988142f0326cb835e004b778c34
Ana Lessa Ferreira <analessaferreira.aa@gmail.com>
2025-10-29 16:53:54 -0300

    Add

---
+++ Espetaculo.java
@@ -0,0 +1,34 @@
+public class Espetaculo {
+    private String nome;
+    private String data;
+    private String hora;
+    private double preco;
+    private boolean[] assentos = new boolean[50];
+
+    public Espetaculo(String nome, String data, String hora, double preco) {
+        this.nome = nome;
+        this.data = data;
+        this.hora = hora;
+        this.preco = preco;
+    }
+
+    public void apresentaAssentos() {
+
+    }
+
+    public Entrada novaEntrada(int tipo, int assento) {
+
+    }
+    
+    public double getPreco(){
+
+    }
+
+    public toSting(){
+
+    }
+
+    private void marcarAssento(int assento){
+        
+    }
+}
---
+++ Pedido.java
@@ -0,0 +1,13 @@
+public class Pedido {
+    public Pedido(){
+
+    }
+
+    public void adicionarEntrada(entrada Entrada){
+
+    }
+
+    public double calcularTotal(){
+        
+    }
+}
