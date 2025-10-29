ce0a9c145961bc172c2f2d6b275c92cab20be659
Julia Oliveira Longhi <julialonghi@MacBook-Air-de-Julia.local>
2025-10-29 16:34:48 -0300

    add

---
+++ Teatro.java
@@ -0,0 +1,26 @@
+public class Teatro {
+    private Pedido carrinho = null;
+    private Espetaculo espetaculoSelecionado = null;
+
+    public Teatro() {}
+
+    public void novaCompra() {
+
+    }
+
+    public void apresentaEspetaculos() {
+
+    }    
+
+    public void selecionaEspetaculos(int numero) {
+
+    }
+
+    public void novaEntrada(int tipo, int assento) {
+
+    }
+
+    public double finalizaCompra(String cpf) {
+
+    }
+}
